162cb8b6981ef8ea541d707a7e85918ecbca703e
NataliShine <natali_96@tut.by>
2022-02-15 17:05:06 +0300

    ДЗ

---
+++ Homework.py
@@ -0,0 +1,135 @@
+#Homework 2.1
+
+print ('Programming', 'Essentials', sep='***', end='***')
+print ('in', 'Python', sep='...')
+
+#Homework 2.2
+
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+
+print("    *     " * 2)
+print("   * *    " * 2)
+print("  *   *   " * 2)
+print(" *     *  " * 2)
+print("***   *** " * 2)
+print("  *   *   " * 2)
+print("  *   *   " * 2)
+print("  *****   " * 2)
+
+#Homework 2.3
+
+print ("""I'm""")
+print ('""Learning""')
+print('"""Python"""')
+
+#Homework 2.4
+
+John=5
+Mary=7
+Adam=10
+print(John, Mary, Adam, sep=',')
+Total_apples=John+Mary+Adam
+print (Total_apples)
+print (John-Mary-Adam)
+print(John*Mary*Adam)
+
+#Homework 2.5
+
+kilometers = 12.25
+miles = 7.38
+miles_to_kilometers = miles*1.61
+kilometers_to_miles = kilometers * (1/1.61)
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
+
+#Homework 2.6
+
+x1=0.0
+x2=1.0
+x3=-1.0
+
+print (3*x1**3-2*x1**2+3*x1-1)
+print (3*x2**3-2*x2**2+3*x2-1)
+print (3*x3**3-2*x3**2+3*x3-1)
+
+#Homework 2.6*
+
+x1=float(input())
+x2=float(input())
+x3=float(input())
+
+print (3*x1**3-2*x1**2+3*x1-1)
+print (3*x2**3-2*x2**2+3*x2-1)
+print (3*x3**3-2*x3**2+3*x3-1)
+
+#Homework 2.7
+
+a=2 
+sec=3600 
+print ('Hours:', a)
+print('Seconds in hours:', a*sec)
+print('Goodbye')
+print('Number in seconds:', 3*sec)
+
+#Homework 2.8
+
+x=int(input())
+y=int(input())
+print (x+y)
+print (x-y)
+print (x/y)
+print (x*y)
+print (x**y)
+print (x//y)
+print (x%y)
+
+if x>0:
+    if y<0:
+        print (y)
+    else:
+        print(x)
+
+#Homework 2.9
+
+x=6
+y=(1/x+1/x+1/x+1/x+1/x)
+print (y)
+
+#Homework 2.10
+
+print ('"Hello World!"')
+
+#Homework 2.11
+
+print('11111', '111111', sep='*')
+
+#Homework 2.12
+
+print (42/4+2*(-2))
+
+#Homework 2.13
+
+print (2014**14)
+
+#Homework 2.14
+
+a=3600
+b=24*a
+c=b*30
+print (c)
+
+#Homework 2.15
+
+print (2014.0**14)
+
+
+
+
